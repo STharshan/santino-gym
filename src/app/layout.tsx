28b4f298615814220440200e724abd47a8a23d17
STharshan <sivatharshan81@gmail.com>
2025-08-12 14:37:00 +0530

    Edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";  // Import the Head component
+// import { Header } from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Santino's Gym & Studio - Your Fitness Your Victory",
-  description: "Ready to change your physique, but can’t find your gym?",
+  description:
+    "Ready to change your physique, but can’t find your gym?",
   generator: "Next.js",
 };
 
@@ -19,12 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Metadata and favicon */}
-        <meta name="description" content="Ready to change your physique, but can’t find your gym?" />
-        <title>Santino's Gym & Studio - Your Fitness Your Victory</title>
-        <link rel="icon" href="/favicon.png" /> {/* Ensure the favicon is correctly placed in public folder */}
-      </Head>
       <body className={inter.className}>
         {/* <Header /> */}
         {children}
